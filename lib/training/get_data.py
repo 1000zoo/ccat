5fd32f3d0d174514e043861074e344a4cf71397f
1000zoo <cjswldn99@gmail.com>
2023-07-13 03:57:39 +0900

    .

--- lib/training/get_data.py
+++ lib/training/get_data.py
@@ -1,10 +1,23 @@
 import pyupbit as pu
+import os
+from lib.etc.private_constants import *
+from lib.etc.util import *
+
 """
 1, 3, 5, 10, 15, 30, 60, 240
 """
 
+
+
 if __name__ == '__main__':
-    c = pu.get_ohlcv("KRW-BTC", count=1, interval='day')
-    print(c)
+    tt = os.path.join(TRAIN_DATA_PATH, "ex")
+    check_dir(tt)
+
+    csv = os.path.join(tt, "test.csv")
+
+    with open(csv, 'w') as f:
+        d = pu.get_ohlcv("KRW-BTC", count=1, interval="day")
+
+
 
 
