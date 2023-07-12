a63a423efe2632764cd518d4a94be2863bffd216
1000zoo <cjswldn99@gmail.com>
2023-07-13 03:16:26 +0900

    base

--- lib/training/get_data.py
+++ lib/training/get_data.py
@@ -1,6 +1,10 @@
 import pyupbit as pu
+"""
+1, 3, 5, 10, 15, 30, 60, 240
+"""
 
-#
-#
-# if __name__ == '__main__':
-#
+if __name__ == '__main__':
+    c = pu.get_ohlcv("KRW-BTC", count=1, interval='day')
+    print(c)
+
+
